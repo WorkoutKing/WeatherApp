3cd0cb2681439a7bc2b0479087b03564474095ea
WorkoutKing <90514083+WorkoutKing@users.noreply.github.com>
2022-01-11 08:05:17 +0200

    Add files via upload

---
+++ src/components/table.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import './style/table.modules.scss'
+
+const Table = () =>{
+
+return(
+    <section id="app">
+    </section>
+)
+}
+
+export default Table
---
+++ src/components/table2.jsx
@@ -0,0 +1,27 @@
+import React from "react"
+
+import './style/table2.modules.scss'
+
+const Table2 = () =>{
+
+return(
+    <>
+    <section id="appas">
+        <div id="datedisplay"></div>
+
+        <div id="temperature" className="temp1 temp"></div>
+        <div id="temperature2" className="temp2 temp"></div>
+        <div id="temperature3" className="temp3 temp"></div>
+        <div id="temperature4" className="temp4 temp"></div>
+        <div id="temperature5" className="temp5 temp"></div>
+        <div id="temperature6" className="temp6 temp"></div>
+        <div id="temperature7" className="temp7 temp"></div>
+    </section>
+    <section id="dateimg">
+        <div id="datedisplay1" clasName="dateimg"></div>
+    </section>
+   </>
+)
+}
+
+export default Table2
